9e1c2463096ba64637f3c4587854cff2daaebed0
Seif <seifeddinejemli@gmail.com>
2017-10-24 23:09:52 +0200

    Adding sort and print for output

--- src/main/java/FileIndexer.java
+++ src/main/java/FileIndexer.java
@@ -1,8 +1,30 @@
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 public class FileIndexer {
 
+    public static LinkedHashMap sortByValue(Map<String, Integer> map) {
 
+        return map.entrySet().stream().sorted(Map.Entry.comparingByValue(Collections.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (key1, key2) -> key1,//merge function to ignore duplicates
+                        LinkedHashMap::new
+                ));
+
+    }
+
+    public static void printMap(Map<String, Integer> map, int numberToPrint) {
+
+        int counter = 0;
+        for (String key : map.keySet()) {
+            System.out.println(key + " - " + map.get(key));
+            if (++counter == 10) break;
+        }
+    }
     public static void main(String[] args) {
 
 //        if (args == null || args.length == 0 ||  args.length > 1){
@@ -24,8 +46,8 @@ public class FileIndexer {
             e.printStackTrace();
         }
         executorService.shutdown();
-        System.out.println("Size" + resultMap.size());
-        System.out.println(resultMap);
+        printMap(sortByValue(resultMap), 10);
+
 
     }
 }
